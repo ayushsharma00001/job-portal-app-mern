727fc4574da918737d96be7bf8259845f12b85f8
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2024-08-12 14:50:54 +0530

    update 6

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://localhost:8080/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8080/api/v1/job"
-export const APPLICATION_API_END_POINT="http://localhost:8080/api/v1/application"
-export const COMPANY_API_END_POINT="http://localhost:8080/api/v1/company"
+export const USER_API_END_POINT="https://40582d9f.jobhunt-c45.pages.dev/api/v1/user";
+export const JOB_API_END_POINT="https://40582d9f.jobhunt-c45.pages.dev/api/v1/job"
+export const APPLICATION_API_END_POINT="https://40582d9f.jobhunt-c45.pages.dev/api/v1/application"
+export const COMPANY_API_END_POINT="https://40582d9f.jobhunt-c45.pages.dev/api/v1/company"
