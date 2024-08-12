64ad3c7de32b8a314c4b5ff0e189888755b40705
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2024-08-12 14:54:09 +0530

    update 7

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://40582d9f.jobhunt-c45.pages.dev/api/v1/user";
-export const JOB_API_END_POINT="https://40582d9f.jobhunt-c45.pages.dev/api/v1/job"
-export const APPLICATION_API_END_POINT="https://40582d9f.jobhunt-c45.pages.dev/api/v1/application"
-export const COMPANY_API_END_POINT="https://40582d9f.jobhunt-c45.pages.dev/api/v1/company"
+export const USER_API_END_POINT="https://jobhuntt.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://jobhuntt.onrender.com/api/v1/job"
+export const APPLICATION_API_END_POINT="https://jobhuntt.onrender.com/api/v1/application"
+export const COMPANY_API_END_POINT="https://jobhuntt.onrender.com/api/v1/company"
