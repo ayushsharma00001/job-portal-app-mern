a3f0dce299992a633103ea88680d88b0c19c6894
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2024-08-12 15:08:02 +0530

    update 8

--- frontend/src/components/admin/AdminJobsTable.jsx
+++ frontend/src/components/admin/AdminJobsTable.jsx
@@ -52,10 +52,6 @@ const AdminJobsTable = () => {
                     <MoreHorizontal />
                   </PopoverTrigger>
                   <PopoverContent className="w-32">
-                    <div onClick={()=>navigate(`/admin/jobs/${job._id}`)} className="flex items-center gap-2 w-fit cursor-pointer">
-                      <Edit2 className="w-4" />
-                      <span>Edit</span>
-                    </div>
                     <div onClick={()=>navigate(`/admin/jobs/${job._id}/applicants`)} className="flex items-center w-fit gap-2 cursor-pointer mt-2">
                       <Eye className="w-4"/>
                       <span>Applicants</span>
